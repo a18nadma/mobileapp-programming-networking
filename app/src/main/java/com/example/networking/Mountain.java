cc1cffb840e87716e83835b591a7c9fd8a91102e
a18nadma <49141885+a18nadma@users.noreply.github.com>
2020-05-06 10:51:45 +0200

    God is dead and we have killed him.

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -2,8 +2,8 @@ package com.example.networking;
 
 public class Mountain {
 
-   String s = new String ("{\
-            "ID\":\"mobilprog_k2\",
+   String s = new String ("{
+           \"ID\":\"mobilprog_k2\",
            \"name\":\"K2\",
            \"type\":\"brom\",
            \"company\":\"\",
@@ -17,31 +17,31 @@ public class Mountain {
                     }
                 },
     {
-            \"ID\":"mobilprog_matterhorn",
-            "name":"Matterhorn",
-            "type":"brom",
-            "company":"",
-            "location":"The Alps",
-            "category":"",
-            "size":4478,
-            "cost":14692,
-            "auxdata":{
-                "wiki":"https://en.wikipedia.org/wiki/Matterhorn",
-                "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Matterhorn_from_Domhütte_-_2.jpg/640px-Matterhorn_from_Domhütte_-_2.jpg"
+            \"ID\":\"mobilprog_matterhorn\",
+                        \"name\":\"Matterhorn\",
+                        \"type\":\"brom\",
+                        \"company\":\"\",
+                        \"location\":\"The Alps\",
+                        \"category\":\"\",
+                        \"size\":4478,
+                        \"cost\":14692,
+                        \"auxdata\":{
+                        \"wiki\":\"https://en.wikipedia.org/wiki/Matterhorn\",
+                        \"img\":\"https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Matterhorn_from_Domhütte_-_2.jpg/640px-Matterhorn_from_Domhütte_-_2.jpg\"
                 }
             },
     {
-            "ID":"mobilprog_mount_everest",
-            "name":"Mount Everest",
-            "type":"brom",
-            "company":"",
-            "location":"The Mahalangur Himal sub-range of the Himalayas",
-            "category":"",
-            "size":8884,
-            "cost":29029,
-            "auxdata":{
-                "wiki":"https://en.wikipedia.org/wiki/Mount_Everest",
-                "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Everest_kalapatthar.jpg/512px-Everest_kalapatthar.jpg"
+                        \"ID\":\"mobilprog_mount_everest\",
+                        \"name\":\"Mount Everest\",
+                        \"type\":\"brom\",
+                        \"company\":\"\",
+                        \"location\":\"The Mahalangur Himal sub-range of the Himalayas\",
+                        \"category\":\"\",
+                        \"size\":8884,
+                        \"cost\":29029,
+                        \"auxdata\":{
+                        \"wiki\":\"https://en.wikipedia.org/wiki/Mount_Everest\",
+                        \"img\":\"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Everest_kalapatthar.jpg/512px-Everest_kalapatthar.jpg\"
             }
     }");
 }
